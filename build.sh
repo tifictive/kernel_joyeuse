58aac1f35b9727e77f11387a85fcedbd0b87e1fb
tifictive <tifictive2@mail.ru>
2020-10-01 22:22:49 +0700

    * Add aosp dtc compiler (for Android 10) * Change scripts to make dtbo.img and dtb binary files * Remove unused dtb for atoll platform * Hidden some warnings when compiling dtb * Some minor changes...

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=aarch64-linux-android-
 export CROSS_COMPILE_ARM32=arm-linux-androideabi-
 export LD_LIBRARY_PATH=$HOME/kernel/proton-clang/lib
-export DTC_EXT=$HOME/kernel/aosp-dtc/dtc
+#export DTC_EXT=$HOME/kernel/aosp-dtc/dtc
 
 make CC=clang $@
