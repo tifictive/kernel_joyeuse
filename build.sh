32c886a02a986d31950240df585586f6a3d617b6
tifictive <tifictive2@mail.ru>
2020-10-05 20:46:15 +0700

    Changes in build.sh script

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-export PATH=$HOME/kernel/proton-clang/bin/:$HOME/kernel/aarch64-linux-android/bin/:$HOME/kernel/arm-linux-androideabi/bin/:${PATH}
+COMMON_PATH="$(cd "$(dirname "$0")/../" >/dev/null 2>&1 && pwd)"
+KERNEL_PATH="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
+
+export PATH=$COMMON_PATH/proton-clang/bin/:$COMMON_PATH/aarch64-linux-android/bin/:$COMMON_PATH/arm-linux-androideabi/bin/:${PATH}
 export ARCH=arm64
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=aarch64-linux-android-
 export CROSS_COMPILE_ARM32=arm-linux-androideabi-
-export LD_LIBRARY_PATH=$HOME/kernel/proton-clang/lib
-#export DTC_EXT=$HOME/kernel/aosp-dtc/dtc
+export LD_LIBRARY_PATH=$COMMON_PATH/proton-clang/lib
 
+cd $KERNEL_PATH
 make CC=clang $@
