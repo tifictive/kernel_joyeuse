5515d59fe4da91ff9dff6be4882e2838a7df1a5d
tifictive <tifictive2@mail.ru>
2020-10-05 23:17:56 +0700

    Remove environment variable for 32-bit cross compiler from 'build.sh' script due to lack of kernel support for VDSO32

--- build.sh
+++ build.sh
@@ -3,12 +3,11 @@
 COMMON_PATH="$(cd "$(dirname "$0")/../" >/dev/null 2>&1 && pwd)"
 KERNEL_PATH="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
 
-export PATH=$COMMON_PATH/proton-clang/bin/:$COMMON_PATH/aarch64-linux-android/bin/:$COMMON_PATH/arm-linux-androideabi/bin/:${PATH}
+export PATH=$COMMON_PATH/proton-clang/bin/:$COMMON_PATH/aarch64-linux-android/bin/:${PATH}
 export ARCH=arm64
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=aarch64-linux-android-
-export CROSS_COMPILE_ARM32=arm-linux-androideabi-
 export LD_LIBRARY_PATH=$COMMON_PATH/proton-clang/lib
 
 cd $KERNEL_PATH
-make CC=clang $@
+make -j$(nproc --all) CC=clang $@
